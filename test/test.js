8337096a166747f2cdd9d8ff12bae56868cda5f1
HafidhRizky <hafidhrzk@gmail.com>
2023-07-30 14:03:05 +0700

    add unit testing

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 const request = require('supertest');
+const casual = require("casual")
 const app = require('../app');
+const db = require('../models');
 
 describe('App', function () {
   it('has the default page', function (done) {
@@ -7,4 +9,58 @@ describe('App', function () {
       .get('/api')
       .expect("Hello There!", done);
   });
-}); 
+});
+
+describe('USER TEST', () => {
+  let userdata = {}
+  beforeEach(async () => {
+    // await db.sequelize.sync({ force: true });
+    userdata = {
+      firstName: casual.first_name,
+      lastName: casual.last_name,
+      birthday: casual.date(format = 'YYYY-MM-DD'),
+      latitude: casual.latitude,
+      longtitude: casual.longitude
+    }
+  });
+
+  describe('POST /api/user', function () {
+    it('creating user', function (done) {
+      request(app)
+        .post('/api/user')
+        .send(userdata)
+        .expect(200)
+        .end(function (err, res) {
+          if (err) {
+            return done(err);
+          }
+          done();
+        });
+    });
+  });
+
+  describe('PUT /api/user/:id', function () {
+    it('updating user', async () => {
+      const { latitude, longtitude, ...payload } = userdata
+      userdata.location = JSON.stringify({ latitude, longtitude })
+      const data = await db.user.create(userdata);
+      request(app)
+        .put(`/api/user/${data.id}`)
+        .send(userdata)
+        .expect(200)
+    });
+  });
+
+  describe('DELETE /api/user/:id', function () {
+    it('deleting user', async () => {
+      const { latitude, longtitude, ...payload } = userdata
+      userdata.location = JSON.stringify({ latitude, longtitude })
+      const data = await db.user.create(userdata);
+      request(app)
+        .del(`/api/user/${data.id}`)
+        .expect(200)
+        .then()
+    });
+  });
+})
+
